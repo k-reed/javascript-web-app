520bd062965dd141dd87c4025fdf38a22cf7729a
k-reed <104884586+k-reed@users.noreply.github.com>
2023-04-29 12:30:09 -0600

    added posts

--- models/Post.js
+++ models/Post.js
@@ -1,15 +1,16 @@
 const postsCollection = require('../db').db().collection("posts")
 const ObjectId = require('mongodb').ObjectId
+const User = require('./User')
 
-let Post = function(data, userid) {
+let Post = function (data, userid) {
     this.data = data
     this.errors = []
     this.userid = userid
 }
 
-Post.prototype.cleanUp = function() {
-    if (typeof(this.data.title) != "string") {this.data.title = ""}
-    if (typeof(this.data.body) != "string") {this.data.body = ""}
+Post.prototype.cleanUp = function () {
+    if (typeof (this.data.title) != "string") { this.data.title = "" }
+    if (typeof (this.data.body) != "string") { this.data.body = "" }
 
     // get rid of any bogus properties
     this.data = {
@@ -20,12 +21,12 @@ Post.prototype.cleanUp = function() {
     }
 }
 
-Post.prototype.validate = function() {
-    if (this.data.title == "") {this.errors.push("You must provide a title.")}
-    if (this.data.body == "") {this.errors.push("You must provide a body.")}
+Post.prototype.validate = function () {
+    if (this.data.title == "") { this.errors.push("You must provide a title.") }
+    if (this.data.body == "") { this.errors.push("You must provide a body.") }
 }
 
-Post.prototype.create = function() {
+Post.prototype.create = function () {
     return new Promise((resolve, reject) => {
         this.cleanUp()
         this.validate()
@@ -38,6 +39,42 @@ Post.prototype.create = function() {
             })
         } else {
             reject(this.errors)
+        }
+    })
+}
+
+Post.findSingleById = function (id) {
+    return new Promise(async function (resolve, reject) {
+        if (typeof (id) != "string" || !ObjectId.isValid(id)) {
+            reject()
+            return
+        }
+        let posts = await postsCollection.aggregate([
+            {$match: {_id: new ObjectId(id)}},
+            {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
+            {$project: {
+                title: 1,
+                body: 1,
+                createdDate: 1,
+                author: {$arrayElemAt: ["$authorDocument", 0]}
+            }}
+        ]).toArray()
+
+        // clean up author property in each post object
+        posts = posts.map(function(post) {
+            post.author = {
+                username: post.author.username,
+                avatar: new User(post.author, true).avatar
+            }
+
+            return post
+        })
+
+        if (posts.length) {
+            console.log(posts[0])
+            resolve(posts[0])
+        } else {
+            reject()
         }
     })
 }
