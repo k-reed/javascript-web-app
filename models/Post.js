565c75068129f5cd3c1e21cc9dcd9a546191f431
k-reed <104884586+k-reed@users.noreply.github.com>
2023-04-29 21:43:31 -0600

    added search backend

--- models/Post.js
+++ models/Post.js
@@ -75,7 +75,7 @@ Post.prototype.actuallyUpdate = function() {
     })
 }
 
-Post.reusablePostQuery = function (uniqueOperations, visitorId) {
+Post.reusablePostQuery = function (uniqueOperations, visitorId, finalOperations = []) {
     return new Promise(async function (resolve, reject) {
         let aggOperations = uniqueOperations.concat([
             {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
@@ -86,12 +86,13 @@ Post.reusablePostQuery = function (uniqueOperations, visitorId) {
                 authorId: "$author",
                 author: {$arrayElemAt: ["$authorDocument", 0]}
             }}
-        ])
+        ]).concat(finalOperations)
         let posts = await postsCollection.aggregate(aggOperations).toArray()
 
         // clean up author property in each post object
         posts = posts.map(function(post) {
             post.isVisitorOwner = post.authorId.equals(visitorId)
+            post.authorId = undefined
 
             post.author = {
                 username: post.author.username,
@@ -144,6 +145,19 @@ Post.delete = function(postIdToDelete, currentUserId) {
             reject()
         }
     })
+}
+
+Post.search = function(searchTerm) {
+    return new Promise(async (resolve, reject) => {
+        if (typeof(searchTerm) == "string") {
+            let posts = await Post.reusablePostQuery([
+                {$match: {$text: {$search: searchTerm}}}
+            ], undefined, [{$sort: {score: {$meta: "textScore"}}}])
+            resolve(posts)
+        } else {
+            reject()
+        }
+    }) 
 }
 
 module.exports = Post
